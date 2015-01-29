1194c83a72f8e3b3991ede594fd30daae1d71a33
benjaminspi <benjamin.spinola@gmx.de>
2015-01-29 16:51:53 +0100

    removed registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable and :omniauthable
-  
-	if Rails.env.production?
-		devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
-	else
-		devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
-	end
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+
+	# if Rails.env.production?
+	# 	devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	# else
+	# 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	# end
 end
